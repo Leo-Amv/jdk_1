617fbfb2ea38bf26a39bdb99df15557cc345e708
Leo <leonidamvrosov@yandex.ru>
2023-09-25 21:21:08 +0500

    btnStart updated

--- src/main/java/ru/gb/xo/SettingsWindow.java
+++ src/main/java/ru/gb/xo/SettingsWindow.java
@@ -1,6 +1,8 @@
 package ru.gb.xo;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SettingsWindow extends JFrame {
     private static final int WINDOW_HEIGHT = 230;
@@ -11,6 +13,13 @@ public class SettingsWindow extends JFrame {
         setLocationRelativeTo(gameWindow);
         setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
         setTitle("Settings");
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameWindow.startNewGame(0,3,3,3);
+                setVisible(false);
+            }
+        });
 
         add(btnStart);
     }
